c6d70e8080ab81fd7e6b27cb2373e871de40a938
joaoreis16 <joaoreis16@ua.pt>
2021-05-26 11:43:38 +0100

    temp

---
+++ projeto/src/components/MusicListItems.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="musicList">
+      <article class="musicItem" v-for="item in items" :key="item.id"> {{item.name}}
+      </article>
+  </div>
+</template>
+
+<script lang="ts">
+
+export default {
+  name: 'MusicListItems',
+  props: {
+    items: Array
+  }
+}
+</script>
+
+<style scoped>
+    .musicList .musicItem {
+
+    }
+</style>
--- projeto/src/views/Tab1.vue
+++ projeto/src/views/Tab1.vue
@@ -13,6 +13,8 @@
       </ion-header>
     <p>Olá zarag</p>
       <ExploreContainer name="Tab 1 page" />
+      <music-list-items :items="[{name:'HOUSTONFORNICATION'},{name:'SKELETONS'}]">
+      </music-list-items>
     </ion-content>
   </ion-page>
 </template>
@@ -20,9 +22,21 @@
 <script lang="ts">
 import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
 import ExploreContainer from '../components/ExploreContainer.vue';
+import MusicListItems from '../components/MusicListItems.vue';
 
 export default  {
   name: 'Tab1',
-  components: { ExploreContainer, IonHeader, IonToolbar, IonTitle, IonContent, IonPage }
+  components: { ExploreContainer, IonHeader, IonToolbar, IonTitle, IonContent, IonPage, MusicListItems }
 }
 </script>
+
+<style >
+tab {
+  position: absolute;
+  width: 375px;
+  height: 83px;
+  left: 1px;
+  top: 993px;
+
+}
+</style> 
